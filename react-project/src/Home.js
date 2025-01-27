62033124d9e0dbc684ce20790d4031956b6689ee
WONG ZHAO XUAN <112739121+wzhaoxuan@users.noreply.github.com>
2025-01-27 07:28:06 +0000

    Make the value reactive

--- react-project/src/Home.js
+++ react-project/src/Home.js
@@ -1,14 +1,16 @@
+import { useState } from "react";
 const Home = () =>{
-    const handleClick = (name, e) => {
-        console.log("Hello" + name, e.target);
+    // useState() is a react hook to allow storing and updating values in components
+    const [name, setName] = useState('zhaoxuan');
+    const handleClick = (name) => {
+        setName('wongzhaoxuan')
+        console.log("Hello" + name);
     }
     return(
         <div className="Home">
             <h1>Welcome to My Portfolio</h1>
-            {/* Set a reference 'handleClick' instead of invoke the function 'handleClick()' directly 
-                To pass parameter to the function, we need to wrap the function into anonymous function
-                Allow an event occur when user click on the button*/}
-            <button onClick={(e) => {handleClick('wzhaoxuan', e)}}>Click me</button>
+            <p>{name}</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
