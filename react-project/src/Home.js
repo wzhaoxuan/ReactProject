6ffd5022bb9a2606c489d21c604584c36dd94664
WONG ZHAO XUAN <112739121+wzhaoxuan@users.noreply.github.com>
2025-01-27 07:38:41 +0000

    Outputting list

--- react-project/src/Home.js
+++ react-project/src/Home.js
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import React, { useState } from "react";
 const Home = () =>{
-    // useState() is a react hook to allow storing and updating values in components
-    const [name, setName] = useState('zhaoxuan');
-    const handleClick = (name) => {
-        setName('wongzhaoxuan')
-        console.log("Hello" + name);
-    }
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'alkvwerjfo', author:'mario', id: 1}, // Properties: title, body, author, id 
+        {title: 'My new website', body: 'alkvwerjfo', author:'mario', id: 2},
+        {title: 'My new website', body: 'alkvwerjfo', author:'mario', id: 3}
+    ]);
+
     return(
-        <div className="Home">
-            <h1>Welcome to My Portfolio</h1>
-            <p>{name}</p>
-            <button onClick={handleClick}>Click me</button>
+        <div classblogs="Home">
+            {/* Use map() to loop through the property of the object */}
+           {blogs.map((blog)=>(
+            // Key is used to keep tracking of each item in the dom as it outputs in React
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Written By {blog.author}</p>
+            </div>
+           ))}
         </div>
     );
 }
