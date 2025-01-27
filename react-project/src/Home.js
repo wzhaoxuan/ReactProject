4ef13e7a32f91f15ccef04018910d45d67c0119e
WONG ZHAO XUAN <112739121+wzhaoxuan@users.noreply.github.com>
2025-01-27 07:13:33 +0000

    Event occur

--- react-project/src/Home.js
+++ react-project/src/Home.js
@@ -1,13 +1,14 @@
 const Home = () =>{
-    const handleClick = (name) => {
-        console.log("Hello" + name);
+    const handleClick = (name, e) => {
+        console.log("Hello" + name, e.target);
     }
     return(
         <div className="Home">
             <h1>Welcome to My Portfolio</h1>
             {/* Set a reference 'handleClick' instead of invoke the function 'handleClick()' directly 
-                To pass parameter to the function, we need to wrap the function into anonymous function*/}
-            <button onClick={() => {handleClick('wzhaoxuan')}}>Click me</button>
+                To pass parameter to the function, we need to wrap the function into anonymous function
+                Allow an event occur when user click on the button*/}
+            <button onClick={(e) => {handleClick('wzhaoxuan', e)}}>Click me</button>
         </div>
     );
 }
