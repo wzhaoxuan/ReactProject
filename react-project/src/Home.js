dd621e2d05f60928f56de3b00d92c6805ef5b9f6
wzhaoxuan <newzxuan213@gmail.com>
2025-01-29 00:10:56 +0800

    Conditional Loading Message

--- react-project/src/Home.js
+++ react-project/src/Home.js
@@ -5,30 +5,31 @@ import BlogList from "./Bloglist";
   
 const Home = () =>{
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
-    const [name, setName] = useState('mario');
 
-    
     // This will run the function every render of the component
     useEffect(() => {
-        // Fetch the data from the url to the server
-        fetch('http://localhost:8000/blogs')
-        // This is a promise, so we need to convert it to json with response object
-        .then(res => {
-            return res.json();
-        })
-        .then((data) => {
-            console.log(data);
-            setBlogs(data);
-        })
+            setTimeout(() => {
+                // Fetch the data from the url to the server
+            fetch('http://localhost:8000/blogs')
+            // This is a promise, so we need to convert it to json with response object
+            .then(res => {
+                return res.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setBlogs(data);
+                setIsPending(false);
+            });
+        }, 1000);
     }, []); // Dependency array: only run the function when the name changes
 
     return(
         <div className="Home">
+            {isPending && <div>Loading...</div>}
             {/* Apply javascript*/}
-           {blogs && <BlogList blogs_props={blogs} title="All Blogs!"/>}
-           <button onClick={() => setName('luigi')}>Change Name</button>
-           <p>{name}</p>
+            {blogs && <BlogList blogs_props={blogs} title="All Blogs!"/>}
         </div>
     );
 }
