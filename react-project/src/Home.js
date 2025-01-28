9e2bd773750dd1d31b358cfc9132140b78637a1c
wzhaoxuan <newzxuan213@gmail.com>
2025-01-28 23:21:17 +0800

    useEffect

--- react-project/src/Home.js
+++ react-project/src/Home.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BlogList from "./Bloglist";
+
+// UseEffect: A hook runs a function every render of the components
   
 const Home = () =>{
     const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'alkvwerjfo', author:'mario', id: 1}, // Properties: title, body, author, id 
+        {title: 'My Portfolio', body: 'alkvwerjfo', author:'mario', id: 1}, // Properties: title, body, author, id 
         {title: 'My new website', body: 'alkvwerjfo', author:'mario', id: 2},
-        {title: 'My new website', body: 'alkvwerjfo', author:'mario', id: 3}
+        {title: 'About me', body: 'alkvwerjfo', author:'mario', id: 3}
     ]);
 
     const handleDelete = (id) =>{
@@ -13,6 +15,12 @@ const Home = () =>{
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs); 
     }
+
+    // This will run the function every render of the component
+    useEffect(() => {
+        console.log("Use Effect ran");
+        console.log(blogs);
+    });
 
     return(
         <div className="Home">
