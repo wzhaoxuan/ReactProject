1d3b5b82a04d1b7907eaa75bf28112c804149d71
wzhaoxuan <newzxuan213@gmail.com>
2025-02-28 23:11:19 +0800

    Handle error

--- react-project/src/Home.js
+++ react-project/src/Home.js
@@ -6,27 +6,39 @@ import BlogList from "./Bloglist";
 const Home = () =>{
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
-
+    const [error, setError] = useState(null);
 
     // This will run the function every render of the component
     useEffect(() => {
             setTimeout(() => {
-                // Fetch the data from the url to the server
+            // Fetch the data from the url to the server
             fetch('http://localhost:8000/blogs')
             // This is a promise, so we need to convert it to json with response object
             .then(res => {
+                if(res.ok){
+                    console.log("Data fetched successfully!");
+                } else {
+                    throw Error("Data not fetched successfully!");
+                }
+
                 return res.json();
             })
             .then((data) => {
                 console.log(data);
                 setBlogs(data);
                 setIsPending(false);
-            });
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.message);
+                setIsPending(false);
+            })
         }, 1000);
     }, []); // Dependency array: only run the function when the name changes
 
     return(
         <div className="Home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {/* Apply javascript*/}
             {blogs && <BlogList blogs_props={blogs} title="All Blogs!"/>}
