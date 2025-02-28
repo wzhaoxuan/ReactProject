dc3e3f2fb1fcf6c34d29562a2449dc7fec2fb973
wzhaoxuan <newzxuan213@gmail.com>
2025-03-01 00:03:28 +0800

    Routing

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,20 +1,26 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// 
 function App() {
   return (
     /* 
       This is an jsx syntax, not a html technically.
       jsx uses className instead of class is because class is reserved for html but the className will display as class in inspect.
-    */
+    */ 
+    <Router>
     <div className="App">
       {/* self-closing tag */}
       <Navbar /> 
       <div className="content">
-        <Home />
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
